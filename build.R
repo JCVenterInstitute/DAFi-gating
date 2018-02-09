75c45bf697418daa7fb5e4222811ef6b1c26b98e
rarsenal <ichang@ichanglt-osx.jcvi.org>
2018-02-09 10:43:17 -0800

    updated build.R

--- build.R
+++ build.R
@@ -4,3 +4,8 @@ devtools::build()
 
 
 devtools::install_github("JCVenterInstitute/DAFi-gating", build_vignettes = TRUE)
+
+library(DAFi)
+SampleData <- system.file("extdata", "sample.fcs",package="DAFi")
+inclusionConfig <- system.file("extdata", "inclusion.config",package="DAFi")
+exclusionConfig <- system.file("extdata", "exclusion.config",package="DAFi")
