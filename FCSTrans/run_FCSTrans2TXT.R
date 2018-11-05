04c729652a8c451c9bfefdb2da949cc54577a060
rarsenal <iychang@uci.edu>
2018-11-05 09:14:16 -0800

    updated dafi_pipline to allow comp input file

--- FCSTrans/run_FCSTrans2TXT.R
+++ FCSTrans/run_FCSTrans2TXT.R
@@ -8,4 +8,14 @@ print(args[1])
 library(methods)
 library(utils)
 source("/var/DAFi-gating/FCSTrans/FCSTrans2TXT.R")
-ipconvert(args[1], verbose=T)
+#ipconvert(args[1], verbose=T)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  print("No compensation file specified")
+  ipconvert(args[1], verbose=T)
+} else if (length(args)==2) {
+  print("Compensation file specified")
+  print(args[2])
+  ipconvert(args[1], args[2], verbose=T)
+}
